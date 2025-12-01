73afb61663b3a89187b2141b41b32d0cff58f444
Edd <1265769349@qq.com>
2025-12-01 16:46:29 +0800

    1

--- Task.cpp
+++ Task.cpp
@@ -1,6 +1,49 @@
 #include "Task.hpp"
 
 TNode::TNode(const Task &t){
-    data = t;
-    next = nullptr;
+    this->data = t;
+    this->next = nullptr;
+}
+
+
+
+TList createList(){
+    return nullptr;
+}
+
+void InsertList(TList &task, const Task &newNode){
+    TNode *node = new TNode(newNode);
+
+    if (task = nullptr)
+    {
+        task = node;
+        return ;
+    }
+
+    TNode *p = task;
+    while (p->next != nullptr) 
+    {
+        p = p->next;
+    }
+    p->next = node;
+}
+
+bool DeleteList(TList &task, int id){
+    if (task = nullptr) return false;
+    if(task->data.id == id){
+        TNode * tmp = task;
+        task = task->next;
+        delete tmp;
+        return true;
+    }
+
+    TNode *p = task;
+    while (p->next != nullptr && p->next->data.id != id)
+        p = p->next;
+    if(p->next == nullptr) return false;
+
+    TNode *tmp = p->next;
+    p->next = tmp->next;
+    delete tmp;
+    return true;   
 }
