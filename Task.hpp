c3916c44b95b0b65a29930c4fbbbaf51610d7526
Edd <40850116+Almbki@users.noreply.github.com>
2025-12-08 08:53:15 +0800

    Refactor Task data structures into Task_Stru class
    Refactor Task data structures into a Task_Stru class with public and private sections. Update node and list structures to use class members.

--- Task.hpp
+++ Task.hpp
@@ -2,48 +2,54 @@
 #define TASK_H
 #include <string>
 
-struct Task_data
+class Task_Stru
 {
-    int id;      //标识
-    std::string title;
-    std::string note;
-    int startline;    //202512011230
-    int deadline;
-    int priority;   //1-10
-    bool finished;
+
+public:
+
+    struct Task_data    //单个任务结构信息组成
+    {
+        int id;      //标识
+        std::string title;
+        std::string note;
+        int startline;    //202512011230
+        int deadline;
+        int priority;   //1-10
+        bool finished;
+    };
+
+    struct TNode_elem        //双向链表元素节点
+    {
+        Task_data task;
+        TNode_elem *next;
+        TNode_elem *prior;
+    };
+
+    struct TList        //头节点
+    {
+        int length;      // 链表长度（不计头节点）
+        TNode_elem *first;   
+        TNode_elem *tail;     
+    };
+
+    using TNode_head = TList*;
+
+private:
+
+    TNode_head head;
+
+public:
+
+    Task_Stru();
+    virtual ~Task_Stru();
+
+    virtual TNode_head createList();                              
+    virtual void InsertNode(TNode_head head, TNode_elem *newNode);     
+    virtual bool DeleteNode(TNode_head head, int id);
+    virtual bool EditNode(TNode_head head, int id);
+    virtual void PrintList(const TNode_head head);
+    virtual void PrintNode(const TNode_elem *elem_node);
 };
-
-
-struct TNode
-{
-    Task_data task;
-    TNode *next;
-    TNode *prior;
-
-    TNode(const Task_data &t);
-
-};
-
-struct TList
-{
-    int length;      // 链表长度（不计头节点）
-    TNode *first;   
-    TNode *tail;     
-};
-
-/*
-typedef struct TreeNode
-{
-    Task data;
-    TreeNode *lchild;
-    TreeNode *rchild;
-}TreeNode, *TaskTree;
-*/
-
-TList *createList();                              
-void InsertNode(TList *head, TNode *newNode);     
-bool DeleteNode(TList *head, int id);
-
 
 
 #endif // TASK_H
