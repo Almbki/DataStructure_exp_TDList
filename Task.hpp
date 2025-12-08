a6d59d723e5e0fbc8c90c092d410b81c069325b3
Edd <1265769349@qq.com>
2025-12-08 09:43:39 +0800

    every Task_data hold a head_node

--- Task.hpp
+++ Task.hpp
@@ -2,12 +2,7 @@
 #define TASK_H
 #include <string>
 
-class Task_Stru
-{
-
-public:
-
-    struct Task_data    //单个任务结构信息组成
+struct Task_data    //单个任务结构信息组成
     {
         int id;      //标识
         std::string title;
@@ -18,21 +13,24 @@ public:
         bool finished;
     };
 
-    struct TNode_elem        //双向链表元素节点
+struct TNode_elem        //双向链表元素节点
     {
         Task_data task;
         TNode_elem *next;
         TNode_elem *prior;
     };
 
-    struct TList        //头节点
+struct TList        //头节点
     {
         int length;      // 链表长度（不计头节点）
         TNode_elem *first;   
         TNode_elem *tail;     
     };
 
-    using TNode_head = TList*;
+using TNode_head = TList*;
+
+class Task_Stru
+{
 
 private:
 
@@ -44,11 +42,13 @@ public:
     virtual ~Task_Stru();
 
     virtual TNode_head createList();                              
-    virtual void InsertNode(TNode_head head, TNode_elem *newNode);     
-    virtual bool DeleteNode(TNode_head head, int id);
-    virtual bool EditNode(TNode_head head, int id);
-    virtual void PrintList(const TNode_head head);
-    virtual void PrintNode(const TNode_elem *elem_node);
+    virtual void InsertNode(TNode_elem *newNode);     
+    virtual bool DeleteNode(int id);
+    virtual bool EditNode(int id);
+    virtual void PrintList() const;
+    virtual void PrintNode(const TNode_elem *elem_node) const;
+
+    TNode_head getHead() const { return head; }
 };
 
 
