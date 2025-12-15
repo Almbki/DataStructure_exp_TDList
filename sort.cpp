78617be0f19352fcaec716435ea481fc13f158ad
Yin-Sh31 <2254263620@qq.com>
2025-12-15 08:47:28 +0800

    change sort

--- sort.cpp
+++ sort.cpp
@@ -1,9 +1,10 @@
 #include "Task.hpp"
 /*指针数组array存排序完的节点指针*/
 
-void Task_Stru::QuickSort(int array[], int length, Callback fanc) // 快速排序并展示
+template <typename fanc>
+void Task_Stru::QuickSort(int array[], int length, fanc fanc) // 快速排序并展示
 {
-    // fanc();
+    int *h = array[0], *t = array[length - 1];
 }
 void Task_Stru::SortBy_ddl()
 {
