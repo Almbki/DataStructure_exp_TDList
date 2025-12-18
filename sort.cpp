fb967ebfe7e079f78080141b8803589191f13d9f
Yin-Sh31 <2254263620@qq.com>
2025-12-18 12:50:40 +0800

    change styel

--- sort.cpp
+++ sort.cpp
@@ -2,41 +2,41 @@
 #include "TaskIO.hpp"
 /*指针数组array存排序完的节点指针*/
 
-template <typename fan>
-void QuickSort(TNode_elem *array[], int length, fan fanc) // 快速排序并展示
+template <typename fun>
+void QuickSort(TNode_elem *array[], int length, fun func) // 快速排序并展示
 {
     if (length < 2)
         return;
     int left = 0, right = length - 1;
     // while (left < right)
     // {
-    //     while (left < right && fanc(array[left], array[right]))
+    //     while (left < right && func(array[left], array[right]))
     //         left++;
-    //     while (left < right && !fanc(array[left], array[right]))
+    //     while (left < right && !func(array[left], array[right]))
     //         right--;
     //     std::swap(array[left], array[right]);
     // }
-    // QuickSort(array, left, fanc);
+    // QuickSort(array, left, func);
     // QuickSort(array + left + 1, length - 1 - left, fanc);
     TNode_elem *pivot = array[(left + right) / 2];
 
     while (left <= right)
     {
-        while (fanc(array[left], pivot))
-            ++left;
-        while (fanc(pivot, array[right]))
-            --right;
+        while (func(array[left], pivot))
+            left++;
+        while (func(pivot, array[right]))
+            right--;
 
         if (left <= right)
         {
             std::swap(array[left], array[right]);
-            ++left;
-            --right;
+            left++;
+            right--;
         }
     }
 
-    QuickSort(array, right + 1, fanc);
-    QuickSort(array + left, length - left, fanc);
+    QuickSort(array, right + 1, func);
+    QuickSort(array + left, length - left, func);
 }
 void Task_Stru::SortBy_ddl()
 {
@@ -52,7 +52,7 @@ void Task_Stru::SortBy_ddl()
               { return a->task.deadline < b->task.deadline; });
     for (auto i : array)
     {
-        //Task_Stru::PrintNode(i);
+        // Task_Stru::PrintNode(i);
         IO::printTaskRow(i->task);
     }
 }
@@ -71,7 +71,7 @@ void Task_Stru::SortBy_priority()
               { return a->task.priority > b->task.priority; });
     for (auto i : array)
     {
-        //Task_Stru::PrintNode(i);
+        // Task_Stru::PrintNode(i);
         IO::printTaskRow(i->task);
     }
 }
